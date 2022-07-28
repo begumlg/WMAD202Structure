c34e4844fecc3f8550112919ea50ae9def536c0e
begumlg <olgunbegum@gmail.com>
2022-07-28 15:25:43 -0700

    assignment done

--- wmad202-assignment4/src/ca/ciccc/wmad202/assignment4/question2/Question2.java
+++ wmad202-assignment4/src/ca/ciccc/wmad202/assignment4/question2/Question2.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Question2 {
+
         public static HashMap<String, HashSet<String>> createHashMap; HashSet<String> words;
         {
             Scanner input = new Scanner(System.in);
